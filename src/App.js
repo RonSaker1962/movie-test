937241fefda70f5eeac04aa2b154d0d7ea94896f
Ron Saker <ronsaker@zahav.net.il>
2024-07-04 15:35:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import { useState, useEffect } from "react";
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
+import WatchList from "./components/WatchList";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [watchlist, setWatchList] = useState([]);
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  const toggleWatchList = (movieId) => {
+    setWatchList((prev) =>
+      prev.includes(movieId)
+        ? prev.filter((id) => id !== movieId)
+        : [...prev, movieId]
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Header></Header>
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/WatchList">Watch List</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <MoviesGrid
+                  movies={movies}
+                  watchlist={watchlist}
+                  toggleWatchList={toggleWatchList}
+                />
+              }
+            ></Route>
+            <Route
+              path="/WatchList"
+              element={
+                <WatchList
+                  movies={movies}
+                  watchList={watchlist}
+                  toggleWatchList={toggleWatchList}
+                />
+              }
+            ></Route>
+          </Routes>
+        </Router>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import '../styles.css';
+
+export default function Header() {
+    return (<div className="Header">
+        <img className="logo" src="logo.png" alt="moviedux" />
+        <h2 className="app-subtitle">ברוכים הבאים</h2>
+    </div>);
+}

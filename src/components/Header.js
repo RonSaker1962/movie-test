26c2a05533bd6a7f4a8a8ad37e162c7331e8c385
Ron Saker <ronsaker@zahav.net.il>
2024-07-04 15:41:56 +0300

    Ron

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
-import '../styles.css';
+import "../styles.css";
 
 export default function Header() {
-    return (<div className="Header">
-        <img className="logo" src="logo.png" alt="moviedux" />
-        <h2 className="app-subtitle">ברוכים הבאים</h2>
-    </div>);
+  return (
+    <div className="Header">
+      <img className="logo" src="logo.png" alt="moviedux" />
+      <h2 className="app-subtitle">Welcome</h2>
+    </div>
+  );
 }
